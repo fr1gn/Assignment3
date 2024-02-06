95ede7b06e850d2569ae22f05e485fe23aed81c0
User <231608@astanait.edu.kz>
2024-02-06 23:21:50 +0600

    start

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.sql.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws SQLException {
+         String conString = "jdbc:postgresql://localhost:5432/simpledb";
+         Connection con = null;
+         Statement statement = null;
+         ResultSet rs = null;
+         try {
+             con = DriverManager.getConnection(conString, "postgres", "231608");
+             statement = con.createStatement();
+             rs = statement.executeQuery("SELECT name, surname, age, gender, course FROM users");
+         } catch (SQLException e) {
+             System.out.println("connection error: " + e.getMessage());
+         }
         }
     }
-}
